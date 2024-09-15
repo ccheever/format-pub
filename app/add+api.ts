da237c5fec20a9dd95fd9718e05df6b6adfd0133
evanbacon <bacon@expo.io>
2024-09-15 17:13:05 +0100

    working

--- app/add+api.ts
+++ app/add+api.ts
@@ -1,16 +1,13 @@
-"use server";
 import { neon } from "@neondatabase/serverless";
 
-let DATABASE_URL =
+const DATABASE_URL =
   "postgresql://format_owner:P4QEedifkyV3@ep-curly-credit-a6uf9pul.us-west-2.aws.neon.tech/format?sslmode=require";
 
-export async function GET(request: Request) {
-  let sql = neon(DATABASE_URL);
-
-  console.log("Log from the add+api endpoint");
-  let r1 = await sql(`INSERT INTO accesses (access_time, notes) VALUES(, 'inserted by API route')`);
-  let r2 = await sql(`SELECT COUNT(*) FROM accesses`);
-  return Response.json({ greeting: "Hello from the add api", r2 });
+export async function GET() {
+  const sql = neon(DATABASE_URL);
+  const r1 = await sql(
+    `INSERT INTO accesses (access_time, notes) VALUES(NOW(), 'inserted by API route')`
+  );
+  const r2 = await sql(`SELECT COUNT(*) FROM accesses`);
+  return Response.json({ greeting: "Hello from the add api", r1, r2 });
 }
-
-// app/actions.ts
